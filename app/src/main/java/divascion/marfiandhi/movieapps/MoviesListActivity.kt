b4feb176bdb7e1c2ac46d4b7fcc2c7514901f2a2
raitoti <marfiandhiputra@gmail.com>
2019-04-04 17:08:18 +0800

    First Commit

---
+++ app/src/main/java/divascion/marfiandhi/movieapps/MoviesListActivity.kt
@@ -0,0 +1,12 @@
+package divascion.marfiandhi.movieapps
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MoviesListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies_list)
+    }
+}
